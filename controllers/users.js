28239094270d1035e7bedf8f69ddbe26163e5498
Kareem Edwards <kareemjedwards54@gmail.com>
2026-05-16 12:10:54 -0400

    Removing the hard code from the errors and putting methods instead

--- controllers/users.js
+++ controllers/users.js
@@ -1,24 +1,51 @@
 const User = require("../models/user");
+const {
+  BAD_REQUEST_STATUS_CODE,
+  INTERNAL_SERVER_ERROR,
+  CREATED,
+  REQUEST_STATUS_OK,
+} = require("../utils/errors");
 
 // GET /users
 
 const getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.status(200).send(users))
+    .then((users) => res.status(REQUEST_STATUS_OK).send(users))
     .catch((err) => {
       console.error(err);
-      return res.status(500).send({ message: err.message });
+      return res.status(INTERNAL_SERVER_ERROR).send({ message: err.message });
     });
 };
 
 const createUser = (req, res) => {
   const { name, avatar } = req.body;
   User.create({ name, avatar })
-    .then((user) => res.status(201).send(user))
+    .then((user) => res.status(CREATED).send(user))
     .catch((err) => {
       console.error(err);
-      return res.status(500).send({ message: err.message });
+      if (err.name === "ValidationError") {
+        return res
+          .status(BAD_REQUEST_STATUS_CODE)
+          .send({ message: err.message });
+      }
+      return res.status(INTERNAL_SERVER_ERROR).send({ message: err.message });
     });
 };
 
-module.exports = { getUsers, createUser };
+const getUser = (req, res) => {
+  const { userId } = req.params;
+  User.findById(userId)
+    .orFail()
+    .then((user) => res.status(REQUEST_STATUS_OK).send(user))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "DocumentNotFoundError") {
+        return res
+          .status(BAD_REQUEST_STATUS_CODE)
+          .send({ message: err.message });
+      }
+      return res.status(INTERNAL_SERVER_ERROR).send({ message: err.message });
+    });
+};
+
+module.exports = { getUsers, createUser, getUser };
