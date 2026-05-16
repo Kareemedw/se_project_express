b5934fab7b2f28ef7cb056b8d62335e8dc93fa99
Kareem Edwards <kareemjedwards54@gmail.com>
2026-05-16 12:20:07 -0400

    Fixing a bug in ESlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: "latest",
-    sourceType: "module",
+    sourceType: "script",
   },
   rules: {
     "no-console": ["warn", { allow: ["error"] }],
